ae56cd468c75cc778bc45a3ac302f307b42ff081
Jacob <312846421@qq.com>
2019-02-26 23:36:05 +0800

    学习react-navigation part5
    createBottomTabNavigator 学习1

--- App.js
+++ App.js
@@ -1,191 +1,95 @@
 import React from 'react';
-import { View, Text, Button, StatusBar, Platform, Image } from 'react-native';
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
-
-
-const isAndroid = Platform.OS === 'android';
-const isiOS = Platform.OS === 'ios';
-
-// modal 方式弹出控制器
+import {Image, Text, View} from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
-
-    static navigationOptions = ({ navigation }) => {
-        return {
-            headerTitle: 'HOME', // 下一页的返回按钮显示 < HOME
-            headerLeft: (
-                <Button
-                    // modal 方式弹出
-                    onPress={() => navigation.navigate('MyModal')}
-                    title="modal方式"
-                    color="#fff"
-                />
-            ),
-        };
-    };
-
     render() {
-
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => {
-                        this.props.navigation.navigate('Details', {
-                            screenTitle: 'DetailsScreen',
-                            otherParam: 'anything you want here',
-                        });
-                    }}
-                />
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
             </View>
         );
     }
 }
 
-class DetailsScreen extends React.Component {
-
-    static navigationOptions = ({ navigation }) => {
-        return {
-            title: navigation.getParam('screenTitle', '默认值标题'),
-        };
-    };
-
+class SettingsScreen extends React.Component {
     render() {
-
-        /* 获取导航时传入的 param, 并提供一个默认值 */
-        const { navigation } = this.props;
-        const screenTitle = navigation.getParam('screenTitle', '默认值标题');
-        const otherParam = navigation.getParam('otherParam', 'some default value');
-
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
-                <Text>screenTitle: {JSON.stringify(screenTitle)}</Text>
-                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-
-                <Button
-                    title="Go back"
-                    onPress={() => this.props.navigation.goBack()}
-                />
-
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
             </View>
         );
     }
 }
 
-class ModalScreen extends React.Component {
+class IconWithBadge extends React.Component {
     render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-                <Button
-                    onPress={() => this.props.navigation.goBack()}
-                    title="Dismiss"
-                />
+        const { name, badgeCount, color, size } = this.props;
+        console.log(size, name, badgeCount, color);
 
-                {/*从 modal 出的界面跳转到原先带导航的界面的下一层，会先 dismiss，然后再 push 到 detailScreen，动画不衔接连续*/}
-                <Button
-                    title="Go to Details"
-                    onPress={() => {
-                        this.props.navigation.navigate('Details', {
-                            screenTitle: 'DetailsScreen',
-                            otherParam: 'anything you want here',
-                        });
-                    }}
+        return (
+            <View style={{ width: size, height: size, margin: 5 }}>
+                <Image
+                    source={require('./Img/user.png')} // 重点 require 内部不能使用变量参数
+                    style={{ width: 20, height: 20 }}
                 />
-                
-                {/*直接返回 modal 这个的源控制器(Home)，则类似于 dismiss 。push 是没效的 */}
-                <Button
-                    title="Go to Home"
-                    onPress={() => {
-                        this.props.navigation.navigate('Home', {
-                            screenTitle: 'DetailsScreen',
-                            otherParam: 'anything you want here',
-                        });
-                    }}
-                />
+                {badgeCount > 0 && (
+                    <View
+                        style={{
+                            // /If you're using react-native < 0.57 overflow outside of the parent
+                            // will not work on Android, see https://git.io/fhLJ8
+                            position: 'absolute',
+                            right: -6,
+                            top: -3,
+                            backgroundColor: 'red',
+                            borderRadius: 6,
+                            width: 12,
+                            height: 12,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                            {badgeCount}
+                        </Text>
+                    </View>
+                )}
             </View>
         );
     }
 }
 
+const HomeIconWithBadge = props => {
+    // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+};
 
+const getTabBarIcon = (navigation, focused, tintColor) => {
+    const { routeName } = navigation.state;
+    let iconName;
+    if (routeName === 'Home') {
+        iconName = focused ? 'like' : 'love';
+    } else if (routeName === 'Settings') {
+        iconName =  focused ? 'like' : 'user';
+    }
+    // You can return any component that you like here!
+    return <HomeIconWithBadge name={iconName} size={25} color={tintColor} />;
+};
 
-const AppNavigator = createStackNavigator(
-    {
-        Home: {
-            screen: HomeScreen,
+export default createAppContainer(
+    createBottomTabNavigator(
+        {
+            Home: { screen: HomeScreen },
+            Settings: { screen: SettingsScreen },
         },
-        Details: {
-            screen: DetailsScreen,
-        },
-    },
-    {
-        initialRouteName: 'Home', // 初始路由
-
-        // 全局导航样式
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#f4511e',
+        {
+            defaultNavigationOptions: ({ navigation }) => ({
+                tabBarIcon: ({ focused, tintColor }) =>
+                    getTabBarIcon(navigation, focused, tintColor),
+            }),
+            tabBarOptions: {
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
             },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 30,
-            },
-    }
-});
-
-
-
-// 第二个 stack，即另一个 navigator
-const RootStack = createStackNavigator(
-    {
-        Main: {
-            screen: AppNavigator,
-        },
-        MyModal: {
-            screen: ModalScreen,
-        },
-    },
-    {
-        mode: 'modal',
-        headerMode: 'none',
-    }
-);
-
-
-// 通过当前状态获取页面标题
-// gets the current screen from navigation state
-function getActiveRouteName(navigationState) {
-    if (!navigationState) {
-        return null;
-    }
-    const route = navigationState.routes[navigationState.index];
-    // dive into nested navigators
-    if (route.routes) {
-        return getActiveRouteName(route);
-    }
-    return route.routeName;
-}
-
-const AppContainer = createAppContainer(RootStack);
-
-// 监听导航器的状态
-export default () => (
-    <AppContainer
-        onNavigationStateChange={(prevState, currentState, action) => {
-            const currentScreen = getActiveRouteName(currentState);
-            const prevScreen = getActiveRouteName(prevState);
-
-            console.log('这是 Action ' + action);
-
-            if (prevScreen !== currentScreen) {
-                // the line below uses the Google Analytics tracker
-                // change the tracker here to use other Mobile analytics SDK.
-                console.log('当前页面 ' + currentScreen);
-            }
-        }}
-    />
+        }
+    )
 );
