e4a2ef1da87d19fab056a2126ac95d461728eeb4
jiabao <liangjiabao@youcash.com>
2019-04-04 18:02:10 +0800

    修改文件目录结构

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, SectionList, Text, Platform, Alert, SafeAreaView, Dimensions, RefreshControl } from 'react-native';
 
-import MovieListScreen from "./TestDemoPage/11-基础组件使用/11.3-SectionList/SectionListPullUp/screen/MovieListScreen";
+import MovieListScreen from "./TestDemoPage/11-基础组件使用/11.2-FlatList/FlatListPullUp/screen/MovieListScreen";
 
 type Props = {};
 export default class App extends Component<Props> {
