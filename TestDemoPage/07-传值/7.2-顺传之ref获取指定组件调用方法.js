64ef2b235a9c55561b9eeb1d3f7e70ee2aabd7c0
jiabao <liangjiabao@youcash.com>
2019-04-08 11:16:45 +0800

    更新ref的使用方式（通过回调函数方式添加组件引用）

--- TestDemoPage/07-传值/7.2-顺传之ref获取指定组件调用方法.js
+++ TestDemoPage/07-传值/7.2-顺传之ref获取指定组件调用方法.js
@@ -12,6 +12,15 @@ import {StyleSheet, Text, View} from 'react-native';
 // 传值
 
 // 2 顺传，有时候需要拿到组件传值，通过ref拿到控件给控件传值。
+/*
+参考：
+1、[对组件的引用（refs） | 论坛 - React Native中文社区](http://bbs.reactnative.cn/topic/608/%E5%AF%B9%E7%BB%84%E4%BB%B6%E7%9A%84%E5%BC%95%E7%94%A8-refs)
+    1.1 _永远_不要在组件的render方法中或者当render方法还在调用栈中任意位置执行的时候——访问引用。此时你得到的是undefined
+    1.2 如果你定义了ref="myRefString'，你必须以this.refs.myRefString 的形式来访问引用
+
+2、[Refs和DOM - React](https://reactjs.org/docs/refs-and-the-dom.html)
+
+*/
 
 class Son extends Component {
 
@@ -46,11 +55,21 @@ class Father extends Component {
 
                 <Text style={styles.clickStyle} onPress={()=>{
                     // 调用ref中保存的控件，注意调用时的名称是refs，多了个s，保存时是ref没有s
-                    this.refs.Son.getMoney(100);
+                    // this.refs.Son.getMoney(100);
+                    this.Son.getMoney(100);
                 }}>点击给son发钱</Text>
 
-                {/* 通过ref拿到Son控件传值 */}
-                <Son ref='Son' name={'子控件Son'}/>
+                {/* 通过ref拿到Son控件传值，再通过refs获取这个控件实例。这种字符串形式现在已经基本被弃用 */}
+                {/*<Son ref='Son' name={'子控件Son'}/>*/}
+
+                {/*
+                只有在组件的render方法被调用时，ref才会被调用，组件才会返回ref。
+                如果你在调用this.refs.xx时render方法还没被调用，那么你得到的是undefined。
+
+                ref 属性不仅接受 string 类型的参数，而且它还接受一个function作为callback，如下方式也是可以的
+                */}
+                <Son ref={(ref) => {this.Son = ref}}/>
+
             </View>
 
         );
